983eb70affe2456e2cd351935b701dbbb4266c69
carlitros900 <carlitros900@gmail.com>
2023-02-24 08:49:11 -0800

    Fire_HD10_9th_Gen-7.3.2.4-20220825

--- build_kernel_config.sh
+++ build_kernel_config.sh
@@ -10,13 +10,16 @@ KERNEL_SUBPATH="kernel/mediatek/mt8183/4.4"
 DEFCONFIG_NAME="maverick_defconfig"
 TARGET_ARCH="arm64"
 TOOLCHAIN_REPO="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9"
+TOOLCHAIN_BRANCH="llvm-r383902b"
 TOOLCHAIN_NAME="aarch64-linux-android-4.9"
 TOOLCHAIN_PREFIX="aarch64-linux-android-"
+PARALLEL_EXECUTION="-j24"
 
 # Expected image files are seperated with ":"
 KERNEL_IMAGES="arch/arm64/boot/Image:arch/arm64/boot/Image.gz:arch/arm64/boot/Image.gz-dtb"
 
 ################################################################################
 # NOTE: You must fill in the following with the path to a copy of Clang compiler
+#       of version r383902b
 ################################################################################
 CLANG_COMPILER_PATH=""
